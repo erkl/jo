4465571825cb5f589c05b3666ac27a50892b463b
Erik Lundin <erik@kvick.co>
2015-07-09 16:48:03 +0100

    Add very basic benchmarking

--- jo_test.go
+++ jo_test.go
@@ -674,3 +674,67 @@ func TestScannerErrors(t *testing.T) {
 		t.Fatalf("Scanner.End did not remember previous error")
 	}
 }
+
+var sample = `
+{
+   "data": [
+      {
+         "id": "X999_Y999",
+         "from": {
+            "name": "Tom Brady", "id": "X12"
+         },
+         "message": "Looking forward to 2010!",
+         "actions": [
+            {
+               "name": "Comment",
+               "link": "http://www.facebook.com/X999/posts/Y999"
+            },
+            {
+               "name": "Like",
+               "link": "http://www.facebook.com/X999/posts/Y999"
+            }
+         ],
+         "type": "status",
+         "created_time": "2010-08-02T21:27:44+0000",
+         "updated_time": "2010-08-02T21:27:44+0000"
+      },
+      {
+         "id": "X998_Y998",
+         "from": {
+            "name": "Peyton Manning", "id": "X18"
+         },
+         "message": "Where's my contract?",
+         "actions": [
+            {
+               "name": "Comment",
+               "link": "http://www.facebook.com/X998/posts/Y998"
+            },
+            {
+               "name": "Like",
+               "link": "http://www.facebook.com/X998/posts/Y998"
+            }
+         ],
+         "type": "status",
+         "created_time": "2010-08-02T21:27:44+0000",
+         "updated_time": "2010-08-02T21:27:44+0000"
+      }
+   ]
+}
+`
+
+func BenchmarkScanner(b *testing.B) {
+	var s = NewScanner()
+	var n = len(sample)
+
+	b.SetBytes(int64(n))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < n; j++ {
+			s.Scan(sample[j])
+		}
+
+		s.End()
+		s.Reset()
+	}
+}
